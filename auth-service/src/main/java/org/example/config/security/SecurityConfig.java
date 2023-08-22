39b8be3f08fd9d9ec2a197579489575922c13a6b
todoshi <ttodoshi@users.noreply.github.com>
2023-08-22 12:57:23 +0300

    fix: move cors configurations to gateway service

--- auth-service/src/main/java/org/example/config/security/SecurityConfig.java
+++ auth-service/src/main/java/org/example/config/security/SecurityConfig.java
@@ -2,22 +2,16 @@ package org.example.config.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Collections;
-import java.util.List;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -28,9 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors()
-                .configurationSource(corsConfigurationSource())
-                .and()
+                .cors(AbstractHttpConfigurer::disable)
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
@@ -64,21 +56,6 @@ public class SecurityConfig {
     public AuthenticationManager noopAuthenticationManager() {
         return authentication -> {
             throw new AuthenticationServiceException("Authentication is disabled");
-        };
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        return new CorsConfigurationSource() {
-            @Override
-            public CorsConfiguration getCorsConfiguration(@NonNull HttpServletRequest request) {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Collections.singletonList(request.getHeader("Origin")));
-                config.setAllowedMethods(List.of("POST", "GET", "OPTIONS", "PUT", "DELETE", "PATCH", "HEAD"));
-                config.setAllowedHeaders(List.of("Content-Type", "Authorization"));
-                config.setAllowCredentials(true);
-                return config;
-            }
         };
     }
 }
---
+++ gateway-service/src/main/java/org/example/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.util.annotation.NonNull;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        return new CorsWebFilter(corsConfigurationSource());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(@NonNull ServerWebExchange exchange) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Collections.singletonList(exchange.getRequest().getHeaders().getOrigin()));
+                config.setAllowedMethods(List.of("POST", "GET", "OPTIONS", "PUT", "DELETE", "PATCH", "HEAD"));
+                config.setAllowedHeaders(List.of("Content-Type", "Authorization"));
+                config.setAllowCredentials(true);
+                return config;
+            }
+        };
+    }
+}
