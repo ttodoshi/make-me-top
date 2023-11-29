f98f032b337079eeb623e7116d58c48e45f09e11
todoshi <ttodoshi@users.noreply.github.com>
2023-11-29 15:05:23 +0300

    fix: remove jpa annotations from dto class

--- person-service/src/main/java/org/example/person/dto/homework/HomeworkRequestStatusDto.java
+++ person-service/src/main/java/org/example/person/dto/homework/HomeworkRequestStatusDto.java
@@ -2,16 +2,8 @@ package org.example.person.dto.homework;
 
 import lombok.Data;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name = "homework_request_status")
 @Data
 public class HomeworkRequestStatusDto {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer statusId;
-    @Column(nullable = false, unique = true)
-    @Enumerated(EnumType.STRING)
     private HomeworkRequestStatusType status;
 }
